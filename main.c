d0cd6d522d4440c56bd1e47626bacb78605fd502
JIGSAW-HJH <hendrikjnr@webmail.co.za>
2022-11-03 21:04:10 +0200

    Committing updates of main.c dot matrix code to repository

--- main.c
+++ main.c
@@ -8,22 +8,38 @@
 #include <xc.h>
 #include "config.h"
 
+#define DIN     LATBbits.LATB0
+#define LOAD    LATBbits.LATB1
+#define CLK     LATBbits.LATB2
+
+
 void main(void) {
     //ONE TIME SETUP:
     TRISB = 0;// make all pins outputs on portB
     
     //Make these pins LOW:
-    LATBbits.LATB0 = 0;
-    LATBbits.LATB1 = 0;
-    LATBbits.LATB2 = 0;
+    DIN = 0;
+    LOAD = 0;
+    CLK = 0;
     //INFINITE LOOP:
     while(1)
     {
-        LATBbits.LATB0 = 0;
-        __delay_ms(250);
-        LATBbits.LATB1 = 0;
-        __delay_ms(250);
-        LATBbits.LATB2 = 0;
-        __delay_ms(250);
+        
     }//end while
 }//end main
+
+void ledTest(void)
+{
+    //16 bit data packet order:
+    //D15 D14 D13 D12 D11 D10 D9 D8 D7 D6 D5 D4 D3 D2 D1 D0
+    //D15 D14 D13 D12         -> Dont Care bits
+    //D11 D10 D9 D8           -> Address bits
+    //D7 D6 D5 D4 D3 D2 D1 D0 -> Data byte
+    
+    //Test all LEDs on the connected matrices:
+    //Display test address: 0xXF    X -> Dont care, thus use 0xFF
+    //Display test Data: 0xFF
+    //Thus we need to send two bytes of 0xFF to max7219 chip
+    
+    
+}
